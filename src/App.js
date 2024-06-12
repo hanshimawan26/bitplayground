c822965fbac24b89c2687820a7738c68f047cebb
hanshimawan26 <hanshimawan26@gmail.com>
2024-06-12 09:14:05 +0700

    remove unity loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import TebakHewan from './pages/games/tebakhewan/Tebakhewan';
 import Layout from './pages/Layout';
 import NavBar from './components/NavBar';
-import gamesList from './gamesList';
 import './App.css';
-import SampleGame from './pages/games/SampleGame';
 import SpotItGame from './pages/games/spotit/SpotItGame';
 import Transportasi from './pages/games/Transportasi';
 
@@ -17,8 +14,6 @@ function App() {
             <Routes>
                 <Route index element={<Home />} />
                 <Route path="game" element={<Layout />}>
-                    <Route path="tebakhewan" element={<TebakHewan name="Tebak Hewan" />} />
-                    <Route path="samplegame" element={<SampleGame name="Sample Game" />} />
                     <Route path="spotit" element={<SpotItGame name="Spot It" />} />
                     <Route path="transportasi" element={<Transportasi name="Transportasi" />} />
                     {/* Add more game routes here */}
